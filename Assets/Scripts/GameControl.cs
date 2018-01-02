eb4a10f9931601cb38867ae9151bfb8a04799cd7
Jon N <jonathan.nathan@gmail.com>
2018-01-02 00:00:45 -0500

    Added scene transitions

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl controller;
+	// Use this for initialization
+	void Awake () {
+		
+        if (controller == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            controller = this;
+        } else if (controller != this)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
